fee08542887e37e8bac7e71543bde4e33d8d9ace
Patryk Strzygocki <patryk53@o2.pl>
2021-02-24 19:10:15 +0100

    Yes

--- venv/django_react/settings.py
+++ venv/django_react/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'webuno',
         'USER': 'postgres',
-        'PASSWORD': 'Nostromo6',
+        'PASSWORD': 'postgres',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
