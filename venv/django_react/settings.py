b42e8b840c0407e5874823b3b5f5d5f75d8162dc
Patryk Strzygocki <49992339+M0rn1ngStur@users.noreply.github.com>
2021-03-03 18:30:41 +0100

    Update settings.py

--- venv/django_react/settings.py
+++ venv/django_react/settings.py
@@ -97,7 +97,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'webuno',
         'USER': 'postgres',
-        'PASSWORD': 'Nostromo6',
+        'PASSWORD': '#',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
